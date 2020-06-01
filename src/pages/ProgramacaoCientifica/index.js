ed15040dcd6452bdc56aeaaa73d0ef294994c762
Gabriel <gmonteeiro@gmail.com>
2020-05-31 22:52:33 -0300

    estilizacao da programacao cientifica

--- src/pages/ProgramacaoCientifica/index.js
+++ src/pages/ProgramacaoCientifica/index.js
@@ -12,14 +12,32 @@ export default function ProgCientifica(){
         api.get('', { /*headers: { Authorization: eventId }*/ })
             .then(response => {
                 console.log(response);
-                //setEvents(response.data);
+                setEvents(response.data);
             })
     }, ); 
 
     return(
+        
+        <div className="event-container">
+            <div className="top-header">
+                <h1>Programação <br/> Científica <span>.</span></h1>
+            </div>
 
-        <div className="top-header">
-            <h1>Programação <br/> Científica <span>.</span></h1>
+            <div className="event-section">
+                <ul>
+                    {events.map(event => (
+                        <li key={event.PalestraId}>
+                            <strong>Palestrante:</strong>
+                            <p>{event.PalestranteNome}</p>
+
+                            <strong>Título:</strong>
+                            <p>{event.DescricaoAtividade}</p>
+
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        
         </div>
         
     ) 
