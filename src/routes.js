77249ad43afd1c7909fa50e0e19fe9deacd541da
Gabriel <gmonteeiro@gmail.com>
2020-06-07 19:54:32 -0300

    removing unusefull files

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'; 
 // para instalar rodar o comando npm install react-router-dom
 
-import Logon from './pages/Logon';
-import Register from './pages/Register';
-import Profile from './pages/Profile';
-import NewIncident from './pages/NewIncident';
+
 import ProgCientifica from './pages/ProgramacaoCientifica';
 
 export default function Routes(){
@@ -14,10 +11,6 @@ export default function Routes(){
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={ ProgCientifica } />
-                <Route path="/register" component={ Register } />
-
-                <Route path="/profile" component={ Profile } />
-                <Route path="/incidents/new" component={ NewIncident } />
                 <Route path="/progcientifica" component={ ProgCientifica } />
             </Switch>
         </BrowserRouter>
