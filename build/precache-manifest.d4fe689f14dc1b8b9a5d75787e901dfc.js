b0fff74bbf2f5644d562111f60b05f621aeabad9
Gabriel <gmonteeiro@gmail.com>
2020-07-19 21:21:31 -0300

    build version

---
+++ build/precache-manifest.d4fe689f14dc1b8b9a5d75787e901dfc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6228c21121b099e9c4e30ba80faf3e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d67feaf1f2fb80eb45b",
+    "url": "/static/css/2.53823a3d.chunk.css"
+  },
+  {
+    "revision": "63b409c91a1574bd6fac",
+    "url": "/static/css/main.1703b381.chunk.css"
+  },
+  {
+    "revision": "6d67feaf1f2fb80eb45b",
+    "url": "/static/js/2.338b17d1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.338b17d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b409c91a1574bd6fac",
+    "url": "/static/js/main.6d1ca545.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "31c60f0e8f99438fd28703a5ea923fce",
+    "url": "/static/media/bg-progcientifica.31c60f0e.jpg"
+  }
+]);
